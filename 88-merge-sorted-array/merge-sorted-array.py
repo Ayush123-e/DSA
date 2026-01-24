20574554841ea8df29a5861845d2c99e880e59ed
Ayush123-e <ayushnbt94@gmail.com>
2026-01-25 00:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (87.89%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        nums1[m:]=[]
+        for i in range(n):
+            nums1.append(nums2[i])
+        nums1.sort()
+
+    
+
+
